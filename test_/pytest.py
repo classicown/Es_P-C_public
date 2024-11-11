48bb9d5f890d854ac5e51f8a046e3bce53d21e8a
classicown <xathuwebse@gmail.com>
2024-11-11 11:11:27 +0100

    Update pytest.py

--- test_/pytest.py
+++ test_/pytest.py
@@ -1,8 +1,6 @@
 # tests/test_baseserv.py
-
 import pytest
 from baseserv import app  # Assurez-vous que l'application Flask est bien importée depuis baseserv.py
-
 @pytest.fixture
 def client():
     """Fixture qui initialise un client de test pour Flask"""

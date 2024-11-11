de3e15107c19ebf24bd1656bd1b4f33ab776bd7a
classicown <xathuwebse@gmail.com>
2024-11-11 13:02:23 +0100

    Update testbypy.py

--- testbypy.py
+++ testbypy.py
@@ -17,20 +17,17 @@ def test_main_page(client):
     response = client.get('/')
     assert response.status_code == 200
     assert "Formulaire" in response.data.decode('utf-8')  # .decode('utf-8') pour traiter la réponse en chaîne
-
  def test_resultat_correct(client):
     """Teste le cas où les réponses 1 et 2 sont cochées"""
     response = client.post('/resultat', data={'reponse1': 'Oui', 'reponse2': 'Oui'})
     assert response.status_code == 200
     # Vérifiez la présence du texte attendu dans le HTML de la réponse
     assert "Vous avez choisi les bonnes réponses" in response.data.decode('utf-8')
-
 def test_resultat_incorrect(client):
     """Teste le cas où les réponses 3 ou 4 sont cochées, donc résultat incorrect"""
     response = client.post('/resultat', data={'reponse3': 'Oui'})
     assert response.status_code == 200
     assert "Vous avez faux" in response.data.decode('utf-8')
-
 def test_missing_answers(client):
     """Teste le cas où aucune réponse n'est cochée"""
     response = client.post('/resultat', data={})
